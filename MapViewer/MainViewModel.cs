19ba6c3a846ed397c4c917423f1206c0f441fa11
oshiruko <bluetiger.ts@gmail.com>
2018-10-17 01:06:58 +0900

    サンプルコードでtpkを読み込んで表示出来た

---
+++ MapViewer/MainViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapViewer
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private string pathText;
+        private string label;
+        private Esri.ArcGISRuntime.Mapping.Map map;
+
+        public string PathText
+        {
+            get
+            {
+                return this.pathText;
+            }
+            set
+            {
+                this.pathText = value;
+                this.OnPropertyChanged(nameof(pathText));
+
+                return;
+            }
+        }
+        public string Label
+        {
+            get
+            {
+                return this.label;
+            }
+            set
+            {
+                this.label = value;
+                this.OnPropertyChanged(nameof(Label));
+
+                return;
+            }
+        }
+        public Esri.ArcGISRuntime.Mapping.Map Map
+        {
+            get
+            {
+                return this.map;
+            }
+            set
+            {
+                this.map = value;
+                this.OnPropertyChanged(nameof(Map));
+
+                return;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = null;
+        protected void OnPropertyChanged(string info)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+    }
+}
--- MapViewer/MainWindow.xaml.cs
+++ MapViewer/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Portal;
+using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.Tasks.Offline;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MapViewer
 {
@@ -20,10 +12,55 @@ namespace MapViewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        MainViewModel vModel;
+
         public MainWindow()
         {
             InitializeComponent();
+            vModel = new MainViewModel();
+            this.DataContext = vModel;
+            KeyDown += MainWindow_KeyDown;
+        }
+
+        private void MainWindow_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if(e.Key == System.Windows.Input.Key.Space)
+            {
+                Mapview.SetViewpointRotationAsync(Mapview.MapRotation + 3);
+            }
+        }
+
+        private void ButtonMapRead(object sender, RoutedEventArgs e)
+        {
+            vModel.Label = MapManager.ReadMap(vModel.PathText);
+            CreateStyledVectorBasemap2();
+        }
+
+        private void CreateStyledVectorBasemap2()
+        {
+            // Path to the local package (.tpk file).
+            string myDocumentsFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string tileCachePath = System.IO.Path.Combine(myDocumentsFolder, "test.tpk");
+
+
+            // Create a tile cache from the local data.
+            TileCache cache = new TileCache(tileCachePath);
+
+
+            // Use the tile cache to create an ArcGISTiledLayer.
+            ArcGISTiledLayer tiledLayer = new ArcGISTiledLayer(cache);
+
+            // Display the tiled layer as a basemap.
+            var map = new Map(new Basemap(tiledLayer));
+
+
+            double initialLatitude = 35.660859;
+            double initialLongitude = 139.795513;
+            double initialScale = 300000;
+            map.InitialViewpoint = new Viewpoint(initialLatitude, initialLongitude, initialScale);
+            vModel.Map = map;
 
         }
+
     }
 }
